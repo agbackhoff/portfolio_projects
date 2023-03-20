eb31008da67009aa29f5dd2bf0f6d03a47629aa9
agbackhoff <ag.backhoff@gmail.com>
2023-03-20 07:34:59 -0600

    Update and rename complex_case_query.sql to complex_case_queries.sql

--- complex_case_queries.sql
+++ complex_case_queries.sql
@@ -13,3 +13,15 @@ SELECT
 FROM matches_spain
 WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
       AND (awayteam_id = 8633 OR hometeam_id = 8633);
+      
+SELECT 
+	c.name AS country,
+    -- Count matches in each of the 3 seasons
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY name;
