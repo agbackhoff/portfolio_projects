278d4f4299d2bee46522ab3e03d1a0af5e695ef9
agbackhoff <ag.backhoff@gmail.com>
2023-03-21 20:08:05 -0600

    Update Intermediate_queries.sql

--- Intermediate_queries.sql
+++ Intermediate_queries.sql
@@ -102,3 +102,24 @@ FROM home
 INNER JOIN away
 ON home.id = away.id;
 
+
+--- Calculate the average number home and away goals scored Legia Warszawa, 
+--- and their opponents, partitioned by the month in each season.
+
+SELECT 
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+         ELSE 'away' END AS warsaw_location,
+	-- Calculate average goals partitioned by season and month
+    AVG(home_goal) OVER(PARTITION BY season, 
+         	EXTRACT(MONTH FROM date)) AS season_mo_home,
+    AVG(away_goal) OVER(PARTITION BY season, 
+            EXTRACT(MONTH FROM date)) AS season_mo_away
+FROM match
+WHERE 
+	hometeam_id = 8673
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
